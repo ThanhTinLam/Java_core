99edfc39d15c5815fcff4bd1f8e52f708c94fb42
ThanhTinLam <lamtin290592@gmail.com>
2022-08-10 21:50:34 -0500

    for loop

---
+++ src/forloop.java
@@ -0,0 +1,9 @@
+ class forloop {
+    static void method(){
+        for (int i=0; i<=100 ;i=i+1){
+        System.out.println(i);}
+    }
+    public static void main(String[] args){
+        method();
+    }
+}
